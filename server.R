886f312e842cf6cd1e071bc69e2fd26fa9cff47c
Eric Wei <ericjwei@gmail.com>
2017-05-24 16:39:30 -0700

    added theme close #1

--- server.R
+++ server.R
@@ -1,26 +1,35 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(plotly)
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+#"eventtypes":["acoustic noise","acoustic_noise","anthropogenic_event","building collapse","chemical explosion","chemical_explosion",
+#"earthquake","experimental explosion","explosion","ice quake",
+#"landslide","mine collapse","mine_collapse","mining explosion","mining_explosion","not reported",
+#"not_reported","nuclear explosion","nuclear_explosion","other event","other_event","quarry",
+#"quarry blast","quarry_blast","rock burst","rockslide","rock_burst","snow_avalanche","sonic boom","sonicboom","sonic_boom"]
+
+
+
+base.url <- "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson" 
+var <- "&eventtype=explosion" 
+count <- "&limit=200"
+response <- GET(paste0(base.url, count)) 
+data <- fromJSON(content(response, "text"))$features %>% flatten()
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+  # output$distPlot <- renderPlot({
+  #   
+  #   # generate bins based on input$bins from ui.R
+  #   x    <- faithful[, 2] 
+  #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  #   
+  #   # draw the histogram with the specified number of bins
+  #   hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  #   
+  # })
   
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,39 @@
 
 library(shiny)
 
+
+map.panel <- tabPanel('Map')
+
+
+scatter.panel <- tabPanel('Scatter')
+
+
+
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
+  navbarPage(
+    theme = shinythemes::shinytheme("sandstone"),
+    'USGS Earthquake Monitoring', 
+    map.panel, scatter.panel))
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+#   fluidPage(
+#   
+#   # Application title
+#   titlePanel("Old Faithful Geyser Data"),
+#   
+#   # Sidebar with a slider input for number of bins 
+#   sidebarLayout(
+#     sidebarPanel(
+#        sliderInput("bins",
+#                    "Number of bins:",
+#                    min = 1,
+#                    max = 50,
+#                    value = 30)
+#     ),
+#     
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#        plotOutput("distPlot")
+#     )
+#   )
+# ))
