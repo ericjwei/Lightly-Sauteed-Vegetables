0c55b03cf35808901973b5e2bb519e6ae4bd6896
Fran Velazquez <fvelaz@uw.edu>
2017-06-01 16:11:27 -0700

    pasted updated franGraph from master

--- scripts/FranGraph.R
+++ scripts/FranGraph.R
@@ -1,9 +1,9 @@
 library(plotly)
 library(shiny)
 library(dplyr)
-#library(RJSONIO)
 library(jsonlite)
 library(httr)
+
 
 latitude <- function(address) {
   url <- "http://maps.google.com/maps/api/geocode/json?address="
@@ -41,40 +41,27 @@ requestTest <- function(address, radius, number) {
   response <- GET(paste0(base.url, var, radius, count))
   info <- fromJSON(content(response, "text"))$features %>% flatten
   data <- as.data.frame(info)
-  #View(info.df)
+  
   return(data)
 }
-#address <- "Seattle"
-#data <- requestTest(address)
-#View(data)
-
-#data.df <- data.frame(do.call('rbind', strsplit(as.character(data$properties.place),'of',fixed=TRUE)))
-#within(data, data.df <- data.frame(do.call('rbind', strsplit(as.character(data$properties.place),'of',fixed=TRUE)))) %>% View()
 
 BuildBarGraph <- function(address, radius, number) {
-data <- requestTest(address, radius, number)
-
-xAxis <- data$properties.place
-yAxis <- data$properties.mag
-text <- 
-m <- list(
-  b = 400 
-)
-p <- plot_ly(data, x = ~xAxis, y = ~yAxis, type = 'bar', text = text,
-             marker = list(color = 'rgb(158,202,225)',
-                           line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
-  layout(title = paste("Earthquakes near", address),
-         xaxis = list(title = "Place", tickangle = 90, face = "bold"),
-         yaxis = list(title = "Magnitude"), autosize = F, width = 1200, height = 1000,
-         margin = m)
-return(p)
+  data <- requestTest(address, radius, number)
+  
+  xAxis <- data$properties.place
+  yAxis <- data$properties.mag
+  text <- data$properties.title
+  
+  m <- list(
+    b = 400 
+  )
+  
+  p <- plot_ly(data, x = ~xAxis, y = ~yAxis, type = 'bar', text = text,
+               marker = list(color = 'rgb(158,202,225)',
+                             line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
+    layout(title = paste("Earthquakes near", address),
+           xaxis = list(title = "Place", tickangle = 90, face = "bold"),
+           yaxis = list(title = "Magnitude"), autosize = F, width = 1200, height = 1000,
+           margin = m)
+  return(p)
 }
-
-  
-  
-  
-  
-  
-  
-  
-  
