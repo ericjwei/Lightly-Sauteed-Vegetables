a0bce1a787e7970c1f06b34f63440ab89320f1de
Fran Velazquez <fvelaz@uw.edu>
2017-06-01 22:14:25 -0700

    changed size of map

--- scripts/Map.R
+++ scripts/Map.R
@@ -3,20 +3,21 @@ library(plotly)
 library(foreign)
 
 map <- function(magnitude = '') {
-
+  
   base.url <- "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
   var <- "&eventtype=earthquake"
   count <- "&limit=100"
   magnitude <- paste0("&minmagnitude=", magnitude)
   response <- GET(paste0(base.url, var, count, magnitude))
-
+  
   data <- fromJSON(content(response, "text"))$features %>% flatten()
-
+  
   geom <- data$geometry.coordinates
   my.matrix <- do.call("rbind", geom)
   df <- as.data.frame(my.matrix, stringsAsFactors = FALSE, col.names = c("longitude", "latitude", "depth"))
   merged.data <- merge(data, df)
-# data <- dget(file = "info")   
+  # data <- dget(file = "info") 
+  
   g <- list(
     scope = 'world',
     projection = list(type = 'equirectangular'),
@@ -28,12 +29,17 @@ map <- function(magnitude = '') {
     subunitwidth = 0.5
   )
   
+  m <- list(
+    l = 50,
+    r = 50
+  )
+  
   p <- plot_geo(merged.data, lat = ~V2, lon = ~V1) %>% 
     add_markers(
       text = ~paste(paste("Magnitude:", properties.mag), sep = "<br />"),
-      color = "light blue", opacity = 0.3, symbol = I("circle"), size = ~properties.mag, hoverinfo = "text"
+      color = "63b8ff", opacity = 0.3, symbol = I("circle"), size = ~properties.mag, hoverinfo = "text"
     ) %>% 
-    layout(title = 'Earthquake', geo = g)
+    layout(autosize = F, width = 1200, height = 900, title = 'Earthquake', margin = m, geo = g)
   return(p)
 }
 # write.dta(merged.data, file = "data.csv")
